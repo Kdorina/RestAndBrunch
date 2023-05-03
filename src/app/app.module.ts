d432a9d28813800a1cb926e4a37587aef372e910
Kdorina <kovacsdorina2@gmail.com>
2023-05-03 15:50:03 +0200

    componensek legenerálása kész

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { PriceComponent } from './price/price.component';
+import { GaleryComponent } from './galery/galery.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    PriceComponent,
+    GaleryComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
